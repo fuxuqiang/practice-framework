38d01ffba2c8b81763db3b9195f84bcbbb13c3df
fuxuqiang <fuxuqiang@outlook.com>
2020-09-13 17:21:29 +0800

    Container类添加newInstance方法

--- src/Container.php
+++ src/Container.php
@@ -6,19 +6,46 @@ class Container
 {
     private static $binds, $instances;
 
-    public static function bind($name, callable $provider)
+    /**
+     * 注册一个绑定
+     */
+    public static function bind(string $name, callable $provider)
     {
         self::$binds[$name] = $provider;
     }
 
-    public static function instance($name, $instance)
+    /**
+     * 注册一个实例到容器中
+     */
+    public static function instance(string $name, $instance)
     {
         self::$instances[$name] = $instance;
     }
 
-    public static function get($name)
+    /**
+     * 从容器中获取注册的内容
+     */
+    public static function get(string $name)
     {
         return self::$instances[$name] ??
             (isset(self::$binds[$name]) ? self::$instances[$name] = call_user_func(self::$binds[$name]) : null);
+    }
+
+    /**
+     * 创建一个类的实例
+     */
+    public static function newInstance(string $concrete)
+    {
+        $reflector = new \ReflectionClass($concrete);
+        if ($constructor = $reflector->getConstructor()) {
+            foreach ($constructor->getParameters() as $param) {
+                if ($class = $param->getClass()) {
+                    $constructorArgs[] = $class->newInstance();
+                }
+            }
+            return new $concrete(...$constructorArgs);
+        } else {
+            return new $concrete;
+        }
     }
 }
